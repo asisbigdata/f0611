70c3f3abc69804983ed32da3ee7440f707c9c933
wayliu0119 <62133800+wayliu0119@users.noreply.github.com>
2020-06-18 20:29:09 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -708,17 +708,14 @@ def sendButton3(event):  #按鈕樣版
             template=ButtonsTemplate(
                 thumbnail_image_url='https://evernote.com/blog/wp-content/uploads/2016/08/Facebook_LinkImage_1200x627-copy-1-300x300.png',  #顯示的圖片
                 title=' ',  #主標題
-                text='查詢特定人士：',  #副標題
+                text='重要資訊：',  #副標題
                 actions=[
-                    MessageTemplateAction(  #顯示文字計息
-                        label='查詢董監事',
-                        text='https://reurl.cc/AqXVEe'
+                URITemplateAction(  #顯示文字計息
+                        label='最終受益人查詢',
+                        uri='https://reurl.cc/lVAQl9'
                     ),
-                    MessageTemplateAction( 
-                        label='查詢公司',
-                        text='https://reurl.cc/AqXVEe'
-                    ),
-                ]
+                    
+		]
             )
         )
         line_bot_api.reply_message(event.reply_token, message)
